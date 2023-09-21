e7f270967c84c340d53356b6235dda0cb2a478d1
Kem <124768041+Kembu10@users.noreply.github.com>
2023-09-21 09:06:57 +0200

    Add files via upload
    First level, Where i had to make a Responsieve code to someones first and last name

---
+++ Level 1/Whats-your-name-7d82a18d-5d05eb0c/name.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "wat is je voornaam?" . PHP_EOL;
+
+$naam = readline() . PHP_EOL;
+
+echo "wat is je achternaam?" . PHP_EOL;
+
+
+$achternaam = readline() . PHP_EOL;
+
+echo 'je volledige naam is:' . $naam . '  ' .  $achternaam 
+
+?>
---
+++ Level 1/Wie-is-daar-aedded43-93e021e7/groet.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hallo mens!" . PHP_EOL;
+
+$iets = readline() . PHP_EOL;
+
+echo "De gebruiker zegt: $iets" . PHP_EOL;
+
+
+
+?>
